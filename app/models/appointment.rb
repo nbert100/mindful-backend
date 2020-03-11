1dcf92ba55a6344d0f7e1b2935935800976ebc85
nbert100 <nbert14@gmail.com>
2020-03-10 22:06:17 -0400

    add associations to models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,6 @@
 class Appointment < ApplicationRecord
+    belongs_to :client
+    belongs_to :provider
+
+    
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,4 @@
 class Client < ApplicationRecord
+    has_many :appointments
+    has_many :providers, through :appointments
 end
