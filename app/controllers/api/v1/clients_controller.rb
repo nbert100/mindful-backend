61c278e9da8c29cdfe550da61ef5e7a6d05bbef4
nbert100 <nbert14@gmail.com>
2020-03-12 22:44:22 -0400

    provider and client controllers

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::ClientsController < ApplicationController
+
+    def index
+        @clients = Client.all 
+        render json: @clients
+    end
+
+    def create
+        @client = Client.new(client_params)
+        if @client.save
+            render json: @client
+        else
+            render json: {error: 'Client information invalid'}
+        end
+    end
+
+    def show
+        @client = Client.find(params[:id])
+        render json: @client
+    end
+
+    def destroy
+        @client = Client.find(params[:id])
+        @client.destroy
+    end
+
+    private
+        def client_params
+            params.require(:client).permit(:first_name, :last_name, :cin, :user_id)
+        end
 end
--- app/controllers/api/v1/providers_controller.rb
+++ app/controllers/api/v1/providers_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::ProvidersController < ApplicationController
+
+    def index
+        @providers = Provider.all 
+        render json: @providers
+    end
+
+    def create
+        @provider = Provider.new(provider_params)
+        if @provider.save
+            render json: @provider
+        else
+            render json: {error: 'Provider information invalid'}
+        end
+    end
+
+    def show
+        @provider = Provider.find(params[:id])
+        render json: @provider
+    end
+
+    def destroy
+        @provider = Provider.find(params[:id])
+        @provider.destroy
+    end
+
+    private
+        def provider_params
+            params.require(:provider).permit(:first_name, :last_name, :specialty)
+        end
 end
